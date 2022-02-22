29ded0ea0ed025affdccbd7dcc13edebe991ae27
Jakov Magazin <magazinjk2@gmail.com>
2022-02-22 17:13:46 +0100

    Title refactor

--- kv-frontend/pages/_app.tsx
+++ kv-frontend/pages/_app.tsx
@@ -3,10 +3,16 @@ import type { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
 import MenuHeader from "../components/MenuHeader";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Block Explorer</title>
+        <meta name="description" content="Block Explorer Seminar" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <MenuHeader />
       <Component {...pageProps} />
       <Footer />
--- kv-frontend/pages/index.tsx
+++ kv-frontend/pages/index.tsx
@@ -17,12 +17,6 @@ const Home = ({
 }: RecentBlocksProps & RecentTransactionProps) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Block Explorer</title>
-        <meta name="description" content="Block Explorer Seminar" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>Block Explorer</h1>
 
